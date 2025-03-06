3efab61f7ea2e433c7ff4b43f29cb954ef050317
Htr09 <icedohector@gmail.com>
2025-03-06 10:26:09 -0700

    Pantallas habitos y add habitos

---
+++ PersonalHabits/app/src/main/java/alcala/jose/personalhabits/AddHabito.kt
@@ -0,0 +1,38 @@
+package alcala.jose.personalhabits
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class AddHabito : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_add_habito)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+        val btnAceptar: Button = findViewById(R.id.btnAceptar)
+        btnAceptar.setOnClickListener {
+            val intent = Intent(this, Habitos::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        val btnCancelar: Button = findViewById(R.id.btnCancelar)
+        btnCancelar.setOnClickListener {
+            val intent = Intent(this, Habitos::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+    }
+}
